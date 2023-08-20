4a90a46dad5387a6c8014dab066a6183eaed8ebf
Kenia16Aparicio <125280290+Kenia16Aparicio@users.noreply.github.com>
2023-08-19 20:10:19 -0600

    Agregación de usuario en reportes
    Agregación de usuario en reportes generales y parametrizados

--- api/helpers/report.php
+++ api/helpers/report.php
@@ -66,6 +66,9 @@ class Report extends FPDF
         $this->cell(20);
         $this->setFont('Arial', '', 10);
         $this->cell(230, 10, 'Fecha/Hora: '.date('d-m-Y H:i:s'), 0, 1, 'C');
+        $this->cell(20);
+        $this->cell(230, 5, 'Usuario: ' . $_SESSION['alias_usuario'], 0, 1, 'C');
+        
         // Se agrega un salto de línea para mostrar el contenido principal del documento.
         $this->ln(10);
     }
--- api/helpers/report2.php
+++ api/helpers/report2.php
@@ -64,9 +64,12 @@ class Report extends FPDF
         // Se ubica la fecha y hora del servidor.
         $this->cell(20);
         $this->setFont('Arial', '', 10);
-        $this->cell(166, 10, 'Fecha/Hora: '.date('d-m-Y H:i:s'), 0, 1, 'C');
+        $this->cell(166, 5, 'Fecha/Hora: '.date('d-m-Y H:i:s'), 0, 1, 'C');
         // Se agrega un salto de línea para mostrar el contenido principal del documento.
-        $this->ln(10);
+        $this->cell(20);
+        $this->cell(166, 5, 'Usuario: ' . $_SESSION['alias_usuario'], 0, 1, 'C');
+         // Se agrega un salto de línea para mostrar el contenido principal del documento.
+         $this->ln(10);
     }
 
     /*
